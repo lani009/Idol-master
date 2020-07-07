172de73e0923f014d644cb1d1ff72cd4cb4840f0
pengm2ng <cjksong@ajou.ac.rk>
2020-07-07 22:09:33 +0900

    test add

--- index.java
+++ index.java
@@ -1,5 +1,19 @@
+import sun.jvm.hotspot.tools.SysPropsDumper;
+
 public class A {
     public A() {
         // 앙dkfjdfjdjfkdkfdkfdkfdkjfdk~~~~~~ fjdkslajflksa
     }
+}
+
+
+
+public class B{
+    
+    int a=10;
+    System.Out.Println(a);
+
+
+
+
 }
