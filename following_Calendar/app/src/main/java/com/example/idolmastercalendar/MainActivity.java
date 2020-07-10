b42f1197bbef1b490426e37757e07e8466e4f63f
harunosora908 <qwqe908@gmail.com>
2020-07-11 02:12:42 +0900

    android custom calendar

---
+++ following_Calendar/app/src/main/java/com/example/idolmastercalendar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.idolmastercalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+public class MainActivity<materialCalendarView> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
+        materialCalenderView.setSelectedDate(CalendarDay.today());
+
+        MaterialCalendarView materialCalendarView;
+        materialCalendarView = findViewById(R.id.calenderView);
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator()
+        );
+
+    }
+
+
+
+
+}
