4fcb11b21a86edadf3efbe25b16f5c44516d2c7c
harunosora908 <qwqe908@gmail.com>
2020-07-11 02:28:54 +0900

    android custom calendar

--- following_Calendar/app/src/main/java/com/example/idolmastercalendar/MainActivity.java
+++ following_Calendar/app/src/main/java/com/example/idolmastercalendar/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity<materialCalendarView> extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
-        materialCalenderView.setSelectedDate(CalendarDay.today());
+        materialCalenderView.setSelectedDate(CalendarDay.today()); /////////
 
-        MaterialCalendarView materialCalendarView;
+        MaterialCalendarView materialCalendarView; ////////
         materialCalendarView = findViewById(R.id.calenderView);
 
         materialCalendarView.addDecorators(
