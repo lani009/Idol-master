e3c0080082433a783dadf1a0d4643d19c7fbd785
Lani Jung <lani009@naver.com>
2020-07-21 01:00:32 +0900

    client code structure upload

--- following_client/src/struct/rest/parser/RestRequest.java
+++ following_client/src/struct/rest/parser/RestRequest.java
@@ -1,10 +1,10 @@
 package struct.rest.parser;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
 import java.util.List;
 
 import exception.PlaceNotFoundException;
