5155f302ddf0c1581e222cd3ac5cad84bd3fd433
Lani Jung <lani009@naver.com>
2020-07-21 00:55:34 +0900

    client code structure upload

---
+++ following_client/src/struct/Review.java
@@ -0,0 +1,31 @@
+package struct;
+
+public class Review {
+    /**
+     * 리뷰 텍스트
+     */
+    private String text;
+    /**
+     * 리뷰 작성자
+     */
+    private String author;
+
+    public Review(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    /**
+     * @return 리뷰 내용
+     */
+    public String getText() {
+        return this.text;
+    }
+
+    /**
+     * @return 리뷰 작성자
+     */
+    public String getAuthor() {
+        return this.author;
+    }
+}
