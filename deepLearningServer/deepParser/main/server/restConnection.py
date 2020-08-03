69e77dba7886d9376c20ab755784f1a82307ed21
Lani Jung <lani009@naver.com>
2020-08-03 13:05:55 +0900

    python docker server middle update

--- deepLearningServer/deepParser/main/server/restConnection.py
+++ deepLearningServer/deepParser/main/server/restConnection.py
@@ -1,18 +1,29 @@
-from socket import *
-
+from socket import socket, AF_INET, SOCK_STREAM
+from io import BytesIO
 class RESTConnection:
     def __init__(self):
         self.PORT = 7575
         self.HOST = ''
-        self.ADDR = (self.PORT, self.HOST)
+        self.ADDR = (self.HOST, self.PORT)
         self.serverSocket = socket(AF_INET, SOCK_STREAM)
         self.serverSocket.bind(self.ADDR)
-        self.serverSocket.listen(100)
-        self.clientSocekt, addr_info = serverSocket.accept()
+
+    def getConnection(self):
+        self.serverSocket.listen()
+        self.clientSocket, addr_info = self.serverSocket.accept()
         print('accept')
         print('--client information--')
-        print(self.clientSocekt)
+        print(self.clientSocket, addr_info)
+        return self
 
 
     def recv(self):
-        return self.serverSocket.recv(65535).decode()
+        length = self.clientSocket.recv(4)
+        length = int.from_bytes(length, "little")
+        print("len", length)
+        return self.clientSocket.recv(length).decode()
+
+    def send(self, text):
+        length = len(text.encode())
+        self.clientSocket.sendall(length.to_bytes(4, byteorder='little'))
+        self.clientSocket.sendall(text.encode())
